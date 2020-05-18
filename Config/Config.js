17348b877c3a6a9b99d2e63d104e2bc876af5088
Shariq Hussain <shariq04_amu@yahoo.com>
2020-05-19 04:12:15 +0530

    Adding Local Chnages

--- Config/Config.js
+++ Config/Config.js
@@ -1,4 +1,4 @@
 export const articles_url = 'https://newsapi.org/v2/top-headlines';
 export const country_code = 'india';
-export const category = 'generall';
+export const category = 'generall updated in local';
 export const _api_key = '9cb9d883e8004f08a8e18daf96a0039c';
